03749bb4b47c6a4f54ea128887d901404c48a414
Wade Wegner <wade.wegner@gmail.com>
2017-02-18 08:00:58 -0700

    Updated to new URLs

--- utils/heroku-create.sh
+++ utils/heroku-create.sh
@@ -15,6 +15,8 @@ prodUrl=`sfdx force:org:describe -u $prodUsername --json | jq -r .sfdxAuthUrl`
 testUrl=`sfdx force:org:describe -u $testUsername --json | jq -r .sfdxAuthUrl`
 hubUrl=`sfdx force:org:describe -u $hubUsername --json | jq -r .sfdxAuthUrl`
 
-sfdx heroku:config:set SALESFORCE_URL=$testUrl --app $stagingName
+sfdx heroku:config:set SFDX_AUTH_URL=$testUrl --app $stagingName
 sfdx heroku:config:set SFDX_DEV_HUB_AUTH_URL=$hubUrl --app $stagingName
-sfdx heroku:config:set SALESFORCE_URL=$prodUrl --app $prodName
+sfdx heroku:config:set SFDX_AUTH_URL=$prodUrl --app $prodName
+
+sfdx heroku:ci:config:set SFDX_DEV_HUB_AUTH_URL=$hubUrl --app $stagingName
