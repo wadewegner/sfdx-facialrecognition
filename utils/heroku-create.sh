4317538567397931a17246dcafcd2334e4117a5b
Wade Wegner <wade.wegner@gmail.com>
2017-02-21 14:23:10 -0800

    Updated app.json

--- utils/heroku-create.sh
+++ utils/heroku-create.sh
@@ -2,14 +2,14 @@ source heroku-config
 
 sfdx heroku:pipelines:setup $pipelineName $repo -y
 
-sfdx heroku:pipelines:add $pipelineName --app $stagingName --stage staging
-sfdx heroku:pipelines:add $pipelineName --app $prodName --stage production
+# sfdx heroku:pipelines:add $pipelineName --app $stagingName --stage staging
+# sfdx heroku:pipelines:add $pipelineName --app $prodName --stage production
 
-sfdx heroku:buildpacks:add https://github.com/heroku/salesforce-buildpack --app $stagingName
-sfdx heroku:buildpacks:add https://github.com/heroku/salesforce-buildpack --app $prodName
+# sfdx heroku:buildpacks:add https://github.com/heroku/salesforce-buildpack --app $stagingName
+# sfdx heroku:buildpacks:add https://github.com/heroku/salesforce-buildpack --app $prodName
 
-sfdx heroku:addons:create salesforce:standard --app $stagingName
-sfdx heroku:addons:create salesforce:standard --app $prodName
+# sfdx heroku:addons:create salesforce:standard --app $stagingName
+# sfdx heroku:addons:create salesforce:standard --app $prodName
 
 prodUrl=`sfdx force:org:describe -u $prodUsername --json | jq -r .sfdxAuthUrl`
 testUrl=`sfdx force:org:describe -u $testUsername --json | jq -r .sfdxAuthUrl`
